0a64406942abdca075059644e9611c8d23fcd563
sherri <sherri@sherricruz.com>
2015-04-23 15:39:20 -0700

    Initial commit

---
+++ binding.js
@@ -0,0 +1,10 @@
+/**
+ * Created by SHERRI on 4/23/15.
+ */
+var app = angular.module('myApp', []);
+app.controller("MainCtrl", function ($scope)
+{
+    $scope.data = {
+        label: "My Button 2"
+    };
+});
