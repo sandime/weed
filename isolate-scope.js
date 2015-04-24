aa4cdbea8e4d4ac7cf408e3b47b9e447a4ed0616
sherri <sherri@sherricruz.com>
2015-04-24 12:20:42 -0700

    created params in getMove function

--- isolate-scope.js
+++ isolate-scope.js
@@ -1,23 +1,31 @@
 /**
  * Created by SHERRI on 4/24/15.
  */
-var app= angular.module("isolateApp", []);
-app.controller("AppCtrl", function($scope, $element){
-    $scope.useMove = function (obj){
-        console.log(obj);
+var app = angular.module("isolateApp", []);
+
+app.controller("AppCtrl", function ($scope, $element)
+{
+    $scope.getMove = function (name, movetype, move)
+    {
+        console.log('' + name + ' performed a ' + movetype +
+        ' ' + move );
     };
-    $scope.movetypes=["Finisher", "Offensive Move", "Defensive Move"];
-    $scope.movetype =$scope.movetypes[0];
+
+    $scope.movetypes 	= ['Finisher', 'Offensive Move', 'Defensive Move'];
+    $scope.movetype 	= $scope.movetypes[0];
 })
-.directive("character", function(){
+
+    .directive("character", function ()
+    {
         return {
-            restrict: "E",
+            restrict: 'E',
             scope: {
-                //gets the value @name and @image in html
-                name:"@",
-                image:"@",
-                movetype: "="
+                name: "@",
+                image: "@",
+                movetype: "=",
+                useMove: '&'
             },
-            templateUrl: 'shield_isolate.html'
+            templateUrl: 'shield_isolate.html',
+            controller: 'AppCtrl'
         }
     });
