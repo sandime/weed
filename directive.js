cf056ef336a5f0a137cbf4eee99facbb314b7f62
sherri <sherri@sherricruz.com>
2015-04-23 21:14:01 -0700

    rolled over, rolled out directive on buttons

--- directive.js
+++ directive.js
@@ -3,29 +3,40 @@
  */
 var app = angular.module('myApp', []);
 //note interactiveBtn is used with a dash in the dom, but interpreted like this in js
-app.directive("interactiveBtn", function(){
+app.directive('interactiveBtn', function ()
+{
     return {
         restrict: 'A',
-        transclude: true,
-        //scope, element, attrs can be called anything, but always have the value of what they're named
         link: function (scope, element, attrs)
         {
+            element.bind('mouseenter', function ()
+            {
+                element[0].innerText = "Rolled Over";
+            });
 
+            element.bind('mouseleave', function ()
+            {
+                element[0].innerText = "Rolled Out";
+            });
         }
     }
-}) //chain by deleting app below and the semicolon here
+})
 
-.directive('walterwhite', function() {
-    return {
-        restrict: 'E',
-        transclude: true,
-        //scope, element, attrs can be called anything, but always have the value of what they're named
-        link: function(scope, element, attrs)
-        {
-            //look at the console to see these
-            console.log(scope);
-            console.log(element);
-            console.log(attrs);
-        }
-    };
-});
+    .directive('walterwhite', function ()
+    {
+        return {
+            restrict: 'E',
+            transclude: true,
+            link: function (scope, element, attrs)
+            {
+                console.log(scope);
+                console.log(element);
+                console.log(attrs);
+            }
+        };
+        /*return {
+         restrict: 'E',
+         transclude: true,
+         template: '<h2>I am Heisenberg</h2>'
+         }*/
+    });
